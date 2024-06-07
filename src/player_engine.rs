8770e83c6134343e679d9f6440432d1cea21493f
zchira <cirkovic@teralogics.com>
2024-06-07 23:38:02 +0200

    close player action

--- src/player_engine.rs
+++ src/player_engine.rs
@@ -1,22 +1,35 @@
-use std::thread::sleep;
-use symphonia::core::{audio::SignalSpec, codecs::DecoderOptions, errors::{Error, Result}, formats::{FormatOptions, FormatReader, SeekMode, SeekTo, Track}, io::MediaSourceStream, meta::MetadataOptions, probe::Hint, units::{Duration, Time}};
-use symphonia::core::codecs::CODEC_TYPE_NULL;
 use crossbeam_channel::{Receiver, Sender};
+use std::thread::sleep;
+use symphonia::core::codecs::CODEC_TYPE_NULL;
+use symphonia::core::{
+    audio::SignalSpec,
+    codecs::DecoderOptions,
+    errors::{Error, Result},
+    formats::{FormatOptions, FormatReader, SeekMode, SeekTo, Track},
+    io::MediaSourceStream,
+    meta::MetadataOptions,
+    probe::Hint,
+    units::{Duration, Time},
+};
 
-use crate::{url_source::UrlSource, pulseaudio::{self, AudioOutput, PulseAudioOutput}};
+use crate::{
+    pulseaudio::{self, AudioOutput, PulseAudioOutput},
+    url_source::UrlSource,
+};
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum PlayerActions {
     Pause,
     Resume,
     Seek(f64),
+    Close,
 }
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum PlayerStatus {
     SendPlaying(bool),
     SendDuration(f64),
-    SendPosition(f64)
+    SendPosition(f64),
 }
 
 pub struct PlayerEngine {
@@ -31,7 +44,7 @@ pub struct PlayerEngine {
 pub struct PlayerState {
     pub playing: bool,
     pub duration: f64,
-    pub position: f64
+    pub position: f64,
 }
 
 impl PlayerEngine {
@@ -40,7 +53,7 @@ impl PlayerEngine {
         rx: Receiver<PlayerActions>,
         tx_status: Sender<PlayerStatus>,
         // rx_status: Receiver<PlayerStatus>
-        ) -> Self {
+    ) -> Self {
         Self {
             reader: None,
             rx,
@@ -58,8 +71,10 @@ impl PlayerEngine {
         let hint = Hint::new();
         let mss = MediaSourceStream::new(source, Default::default());
 
-        let format_opts =
-            FormatOptions { enable_gapless: true, ..Default::default() };
+        let format_opts = FormatOptions {
+            enable_gapless: true,
+            ..Default::default()
+        };
         let metadata_opts: MetadataOptions = Default::default();
         let track = None;
 
@@ -68,31 +83,23 @@ impl PlayerEngine {
                 let decode_opts = Default::default();
                 self.reader = Some(probed.format);
                 self.play(track, &decode_opts)
-            },
+            }
             Err(e) => {
                 println!("input not supported: {:#?}", e);
                 Err(e)
-            },
+            }
         }
     }
 
-    fn play(&mut self,
-        track_num: Option<usize>,
-        decode_opts: &DecoderOptions,
-    ) -> Result<i32> {
-
+    fn play(&mut self, track_num: Option<usize>, decode_opts: &DecoderOptions) -> Result<i32> {
         if let Some(reader) = self.reader.as_mut() {
-
             let track = track_num
                 .and_then(|t| reader.tracks().get(t))
                 .or_else(|| first_supported_track(reader.tracks()));
 
             let track_id = track.unwrap().id;
             let mut audio_output = None;
-            let result = self.play_track(
-                &mut audio_output,
-                track_id,
-                decode_opts);
+            let result = self.play_track(&mut audio_output, track_id, decode_opts);
 
             // Flush the audio output to finish playing back any leftover samples.
             if let Some(audio_output) = audio_output.as_mut() {
@@ -100,7 +107,6 @@ impl PlayerEngine {
             }
             result
         } else {
-
             Ok(0)
         }
     }
@@ -111,7 +117,6 @@ impl PlayerEngine {
         track_id: u32,
         decode_opts: &DecoderOptions,
     ) -> Result<i32> {
-
         let rx = self.rx.clone();
         let (tb, dur, mut decoder) = if let Some(r) = self.reader.as_mut() {
             let track = match r.tracks().iter().find(|track| track.id == track_id) {
@@ -120,14 +125,20 @@ impl PlayerEngine {
             };
 
             // Create a decoder for the track.
-            let decoder = symphonia::default::get_codecs().make(&track.codec_params, decode_opts)?;
+            let decoder =
+                symphonia::default::get_codecs().make(&track.codec_params, decode_opts)?;
 
             // Get the selected track's timebase and duration.
             let tb = track.codec_params.time_base;
-            let dur = track.codec_params.n_frames.map(|frames| track.codec_params.start_ts + frames);
+            let dur = track
+                .codec_params
+                .n_frames
+                .map(|frames| track.codec_params.start_ts + frames);
             (tb, dur, decoder)
         } else {
-            return Err(symphonia::core::errors::Error::IoError(std::io::Error::new(std::io::ErrorKind::Other, "")));
+            return Err(symphonia::core::errors::Error::IoError(
+                std::io::Error::new(std::io::ErrorKind::Other, ""),
+            ));
         };
 
         let mut playing = true;
@@ -137,10 +148,8 @@ impl PlayerEngine {
                 Ok(a) => {
                     println!("Action recv: {:#?}", a);
                     Some(a)
-                },
-                Err(_e) => {
-                    None
-                },
+                }
+                Err(_e) => None,
             };
 
             let a = action.clone();
@@ -169,7 +178,9 @@ impl PlayerEngine {
                     Err(err) => break Err(err),
                 }
             } else {
-                return Err(symphonia::core::errors::Error::IoError(std::io::Error::new(std::io::ErrorKind::Other, "")));
+                return Err(symphonia::core::errors::Error::IoError(
+                    std::io::Error::new(std::io::ErrorKind::Other, ""),
+                ));
             };
 
             // If the packet does not belong to the selected track, skip it.
@@ -193,8 +204,7 @@ impl PlayerEngine {
 
                         // Try to open the audio output.
                         audio_output.replace(try_open(spec, duration).unwrap());
-                    }
-                    else {
+                    } else {
                         // TODO: Check the audio spec. and duration hasn't changed.
                     }
 
@@ -212,14 +222,24 @@ impl PlayerEngine {
                     let a = action.clone();
                     if a.is_some() {
                         let a = a.as_ref().unwrap();
-                        match a{
+                        match a {
                             PlayerActions::Seek(ref t) => {
                                 let ts: Time = t.clone().into(); // packet.ts() + 30;
                                 if let Some(reader) = self.reader.as_mut() {
-                                    let r = reader.seek(SeekMode::Accurate, SeekTo::Time{ time: ts, track_id: Some(0) });
+                                    let r = reader.seek(
+                                        SeekMode::Accurate,
+                                        SeekTo::Time {
+                                            time: ts,
+                                            track_id: Some(0),
+                                        },
+                                    );
                                     println!("Seek result: {:#?}", r);
                                 }
-                            },
+                            }
+                            PlayerActions::Close => {
+                                let s = self.tx_status.send(PlayerStatus::SendPlaying(false));
+                                break Ok(());
+                            }
                             _ => {}
                         }
                     }
@@ -239,7 +259,6 @@ impl PlayerEngine {
         Ok(0)
     }
 
-
     // pub fn position_display(&self) -> String {
     //     let position = self.state.read().unwrap().position;
     //     let hours = position / (60.0 * 60.0);
@@ -248,7 +267,12 @@ impl PlayerEngine {
     //     format!("{}:{:0>2}:{:0>4.1}", hours, mins, secs)
     // }
 
-    fn print_progress(&mut self, ts: u64, dur: Option<u64>, tb: Option<symphonia::core::formats::prelude::TimeBase>) {
+    fn print_progress(
+        &mut self,
+        ts: u64,
+        dur: Option<u64>,
+        tb: Option<symphonia::core::formats::prelude::TimeBase>,
+    ) {
         if let Some(tb) = tb {
             let t = tb.calc_time(ts);
 
@@ -267,7 +291,6 @@ impl PlayerEngine {
             println!("::::> {}:{:0>2}:{:0>4.1}", hours, mins, secs);
         }
     }
-
 }
 
 fn ignore_end_of_stream_error(result: Result<()>) -> Result<()> {
@@ -275,11 +298,11 @@ fn ignore_end_of_stream_error(result: Result<()>) -> Result<()> {
         Err(Error::IoError(err))
             if err.kind() == std::io::ErrorKind::UnexpectedEof
                 && err.to_string() == "end of stream" =>
-            {
-                // Do not treat "end of stream" as a fatal error. It's the currently only way a
-                // format reader can indicate the media is complete.
-                Ok(())
-            }
+        {
+            // Do not treat "end of stream" as a fatal error. It's the currently only way a
+            // format reader can indicate the media is complete.
+            Ok(())
+        }
         _ => result,
     }
 }
@@ -288,7 +311,11 @@ pub fn try_open(spec: SignalSpec, duration: Duration) -> pulseaudio::Result<Box<
     PulseAudioOutput::try_open(spec, duration)
 }
 
-fn update_progress(ts: u64, dur: Option<u64>, tb: Option<symphonia::core::formats::prelude::TimeBase>) -> (f64, f64) {
+fn update_progress(
+    ts: u64,
+    dur: Option<u64>,
+    tb: Option<symphonia::core::formats::prelude::TimeBase>,
+) -> (f64, f64) {
     if let Some(tb) = tb {
         let t = tb.calc_time(ts);
         let position = t.frac + t.seconds as f64;
@@ -303,5 +330,7 @@ fn update_progress(ts: u64, dur: Option<u64>, tb: Option<symphonia::core::format
 }
 
 fn first_supported_track(tracks: &[Track]) -> Option<&Track> {
-    tracks.iter().find(|t| t.codec_params.codec != CODEC_TYPE_NULL)
+    tracks
+        .iter()
+        .find(|t| t.codec_params.codec != CODEC_TYPE_NULL)
 }
